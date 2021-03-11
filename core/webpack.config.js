391ea99fa60c6c88f69cb66f790a131e740745e0
pratikkanungo <31898590+pratikkanungo@users.noreply.github.com>
2021-03-11 11:45:34 +0400

    Update webpack.config.js

--- core/webpack.config.js
+++ core/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://127.0.0.1:8080'
+            apiUrl: 'http://192.168.33.10:8080'
         })
     }
 };
